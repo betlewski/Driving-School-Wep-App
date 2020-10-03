f4198aeecf9d0e9bb2e978b9ab3ff4eeb688d640
betlewski <szymonbetlewski@wp.pl>
2020-10-03 22:04:33 +0200

    Added Spring Boot application sources

---
+++ src/test/java/com/project/webapp/drivingschool/DrivingSchoolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.webapp.drivingschool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DrivingSchoolApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
