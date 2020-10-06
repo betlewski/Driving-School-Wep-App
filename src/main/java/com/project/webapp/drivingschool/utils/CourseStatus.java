27523d0d27e5c869316d55581ed1a6c281e5d967
betlewski <szymonbetlewski@wp.pl>
2020-10-06 09:16:04 +0200

    Added enum class for the course process

---
+++ src/main/java/com/project/webapp/drivingschool/utils/CourseStatus.java
@@ -0,0 +1,45 @@
+package com.project.webapp.drivingschool.utils;
+
+public enum CourseStatus {
+
+    /**
+     * Przeprowadzenie badań lekarskich
+     */
+    MEDICAL_EXAMS,
+
+    /**
+     * Złożenie dokumentu PKK
+     */
+    DOCUMENTS_SUBMISSION,
+
+    /**
+     * Zajęcia teoretyczne - wykłady
+     */
+    LECTURES,
+
+    /**
+     * Teoretyczny egzamin wewnętrzny
+     */
+    THEORY_INTERNAL_EXAM,
+
+    /**
+     * Zajęcia praktyczne - jazdy szkoleniowe
+     */
+    DRIVING_TRAININGS,
+
+    /**
+     * Praktyczny egzamin wewnętrzny
+     */
+    PRACTICE_INTERNAL_EXAM,
+
+    /**
+     * Egzaminy państwowe
+     */
+    STATE_EXAMS,
+
+    /**
+     * Odebranie prawa jazdy i zakończenie kursu
+     */
+    COURSE_ENDING
+
+}
