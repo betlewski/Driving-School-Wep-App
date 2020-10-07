4b9f45e8334c0a5dd7ccfc7836c1a29c0d0cc538
betlewski <szymonbetlewski@wp.pl>
2020-10-07 14:25:40 +0200

    Added entity for theory lessons

---
+++ src/main/java/com/project/webapp/drivingschool/model/TheoryLessons.java
@@ -0,0 +1,41 @@
+package com.project.webapp.drivingschool.model;
+
+import com.project.webapp.drivingschool.utils.LessonStatus;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+
+/**
+ * Klasa reprezentująca zajęcia teoretyczne przypisane do kursu
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class TheoryLessons {
+
+    /**
+     * Identyfikator zajęć
+     */
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    /**
+     * Cykl wykładów, w ramach których odbywane są zajęcia
+     */
+    @NotNull
+    @OneToOne
+    private LectureSeries lectureSeries;
+
+    /**
+     * Status przebiegu zajęć
+     */
+    @NotNull
+    @Enumerated(EnumType.STRING)
+    private LessonStatus lessonStatus;
+
+}
