c1b073e0bf395d2e8c8bfc6d39355d72dba17239
betlewski <szymonbetlewski@wp.pl>
2020-10-20 22:38:27 +0200

    Used service methods in document controller

--- src/main/java/com/project/webapp/drivingschool/controller/DocumentController.java
+++ src/main/java/com/project/webapp/drivingschool/controller/DocumentController.java
@@ -1,10 +1,15 @@
 package com.project.webapp.drivingschool.controller;
 
+import com.project.webapp.drivingschool.model.Course;
+import com.project.webapp.drivingschool.model.Document;
 import com.project.webapp.drivingschool.service.DocumentService;
+import com.project.webapp.drivingschool.utils.ProcessingStatus;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+import java.util.Set;
 
 /**
  * Kontroler dla dokumentów wymaganych w trakcie kursu
@@ -19,6 +24,54 @@ public class DocumentController {
     @Autowired
     public DocumentController(DocumentService documentService) {
         this.documentService = documentService;
+    }
+
+    @GetMapping("/all/studentId")
+    @ResponseBody
+    public Set<Document> getAllDocumentsForActiveCourseByStudentId(@RequestParam("id") Long id) {
+        return documentService.getAllDocumentsForActiveCourseByStudentId(id);
+    }
+
+    @GetMapping("/all/studentIdAndStatus")
+    @ResponseBody
+    public Set<Document> getDocumentsForActiveCourseByStudentIdAndProcessingStatus(@RequestParam("id") Long id,
+                                                                                   @RequestParam("status") ProcessingStatus status) {
+        return documentService.getDocumentsForActiveCourseByStudentIdAndProcessingStatus(id, status);
+    }
+
+    @GetMapping("/course")
+    @ResponseBody
+    public Optional<Course> findCourseByDocumentId(@RequestParam("id") Long id) {
+        return documentService.findCourseByDocumentId(id);
+    }
+
+    @GetMapping("/check/medicalExams")
+    @ResponseBody
+    public Boolean checkIfMedicalExamsCompleted(@RequestBody Course course) {
+        return documentService.checkIfMedicalExamsCompleted(course);
+    }
+
+    @GetMapping("/check/all")
+    @ResponseBody
+    public Boolean checkIfAllDocumentsCompleted(@RequestBody Course course) {
+        return documentService.checkIfAllDocumentsCompleted(course);
+    }
+
+    @PostMapping("/add")
+    public ResponseEntity<Document> addDocument(@RequestBody Document document,
+                                                @RequestParam("id") Long id) {
+        return documentService.addDocument(document, id);
+    }
+
+    @DeleteMapping("/delete")
+    public ResponseEntity<Boolean> deleteDocument(@RequestParam("id") Long id) {
+        return documentService.deleteDocument(id);
+    }
+
+    @PutMapping("/changeStatus")
+    public ResponseEntity<Document> changeProcessingStatusByDocumentId(@RequestParam("id") Long id,
+                                                                       @RequestBody ProcessingStatus status) {
+        return documentService.changeProcessingStatusByDocumentId(id, status);
     }
 
 }
