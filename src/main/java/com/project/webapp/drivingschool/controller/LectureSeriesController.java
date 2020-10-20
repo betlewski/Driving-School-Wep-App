d8454435c5671907fc1f812f2902481583097c2c
betlewski <szymonbetlewski@wp.pl>
2020-10-20 22:56:34 +0200

    Used service methods in lecture series controller

--- src/main/java/com/project/webapp/drivingschool/controller/LectureSeriesController.java
+++ src/main/java/com/project/webapp/drivingschool/controller/LectureSeriesController.java
@@ -1,10 +1,14 @@
 package com.project.webapp.drivingschool.controller;
 
+import com.project.webapp.drivingschool.model.LectureSeries;
 import com.project.webapp.drivingschool.service.LectureSeriesService;
+import com.project.webapp.drivingschool.utils.LectureSeriesStatus;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Set;
 
 /**
  * Kontroler dla cyklu wykładów w szkole jazdy
@@ -19,6 +23,30 @@ public class LectureSeriesController {
     @Autowired
     public LectureSeriesController(LectureSeriesService lectureSeriesService) {
         this.lectureSeriesService = lectureSeriesService;
+    }
+
+    @GetMapping("/all/free")
+    @ResponseBody
+    public List<LectureSeries> getAllLectureSeriesPossibleToJoin() {
+        return lectureSeriesService.getAllLectureSeriesPossibleToJoin();
+    }
+
+    @GetMapping("/all/employeeId")
+    @ResponseBody
+    public List<LectureSeries> getAllLectureSeriesByEmployeeId(@RequestParam("id") Long id) {
+        return lectureSeriesService.getAllLectureSeriesByEmployeeId(id);
+    }
+
+    @PostMapping("/add")
+    public ResponseEntity<LectureSeries> addLectureSeries(@RequestParam("id") Long employeeId,
+                                                          @RequestBody Set<Long> lecturesIds) {
+        return lectureSeriesService.addLectureSeries(employeeId, lecturesIds);
+    }
+
+    @PutMapping("/changeStatus")
+    public ResponseEntity<LectureSeries> changeStatusByLectureSeriesId(@RequestParam("id") Long id,
+                                                                       @RequestBody LectureSeriesStatus status) {
+        return lectureSeriesService.changeStatusByLectureSeriesId(id, status);
     }
 
 }
