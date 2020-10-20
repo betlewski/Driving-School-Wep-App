07ecfe74d3aac46e786b0b163718d2b4602d3b7a
betlewski <szymonbetlewski@wp.pl>
2020-10-20 23:30:17 +0200

    Used service methods in student controller

--- src/main/java/com/project/webapp/drivingschool/controller/StudentController.java
+++ src/main/java/com/project/webapp/drivingschool/controller/StudentController.java
@@ -1,10 +1,14 @@
 package com.project.webapp.drivingschool.controller;
 
+import com.project.webapp.drivingschool.model.Course;
+import com.project.webapp.drivingschool.model.Student;
 import com.project.webapp.drivingschool.service.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 /**
  * Kontroler dla kursantów (klientów szkoły nauki jazdy)
@@ -19,6 +23,47 @@ public class StudentController {
     @Autowired
     public StudentController(StudentService studentService) {
         this.studentService = studentService;
+    }
+
+    @GetMapping("/all")
+    @ResponseBody
+    public List<Student> getAllStudents() {
+        return studentService.getAllStudents();
+    }
+
+    @GetMapping("/byEmail")
+    @ResponseBody
+    public Student getStudentByEmail(@RequestParam("email") String email) {
+        return studentService.getStudentByEmail(email);
+    }
+
+    @GetMapping("/byCourse")
+    @ResponseBody
+    public Optional<Student> findStudentByCourse(@RequestBody Course course) {
+        return studentService.findStudentByCourse(course);
+    }
+
+    @GetMapping("/checkEmail")
+    @ResponseBody
+    public Boolean emailExisting(@RequestParam("email") String email) {
+        return studentService.emailExisting(email);
+    }
+
+    @PostMapping("/add")
+    public ResponseEntity<Student> addStudent(@RequestBody Student student) {
+        return studentService.addStudent(student);
+    }
+
+    @PutMapping("/edit")
+    public ResponseEntity<Student> editStudent(@RequestParam("email") String email,
+                                               @RequestBody Student newStudent) {
+        return studentService.editStudent(email, newStudent);
+    }
+
+    @PutMapping("/setPkk")
+    public ResponseEntity<Student> setStudentPkk(@RequestParam("email") String email,
+                                                 @RequestBody String pkk) {
+        return studentService.setStudentPkk(email, pkk);
     }
 
 }
