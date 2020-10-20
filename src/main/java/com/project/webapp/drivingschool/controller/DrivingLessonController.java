0fd87439b036856877ea5d143aa46193f9cf8946
betlewski <szymonbetlewski@wp.pl>
2020-10-20 22:46:49 +0200

    Used service methods in driving lesson controller

--- src/main/java/com/project/webapp/drivingschool/controller/DrivingLessonController.java
+++ src/main/java/com/project/webapp/drivingschool/controller/DrivingLessonController.java
@@ -1,10 +1,15 @@
 package com.project.webapp.drivingschool.controller;
 
+import com.project.webapp.drivingschool.model.Course;
+import com.project.webapp.drivingschool.model.DrivingLesson;
 import com.project.webapp.drivingschool.service.DrivingLessonService;
+import com.project.webapp.drivingschool.utils.LessonStatus;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+import java.util.Set;
 
 /**
  * Kontroler dla zajęć praktycznych (jazd szkoleniowych)
@@ -19,6 +24,48 @@ public class DrivingLessonController {
     @Autowired
     public DrivingLessonController(DrivingLessonService drivingLessonService) {
         this.drivingLessonService = drivingLessonService;
+    }
+
+    @GetMapping("/all/studentId")
+    @ResponseBody
+    public Set<DrivingLesson> getAllDrivingLessonsByStudentId(@RequestParam("id") Long id) {
+        return drivingLessonService.getAllDrivingLessonsByStudentId(id);
+    }
+
+    @GetMapping("/all/employeeId")
+    @ResponseBody
+    public Set<DrivingLesson> getAllDrivingLessonsByEmployeeId(@RequestParam("id") Long id) {
+        return drivingLessonService.getAllDrivingLessonsByEmployeeId(id);
+    }
+
+    @GetMapping("/passedHours")
+    @ResponseBody
+    public Integer getAllPassedHoursOfDrivingLessonsByCourse(@RequestBody Course course) {
+        return drivingLessonService.getAllPassedHoursOfDrivingLessonsByCourse(course);
+    }
+
+    @GetMapping("/isPassed")
+    @ResponseBody
+    public Boolean isDrivingLessonsPassedByCourse(@RequestBody Course course) {
+        return drivingLessonService.isDrivingLessonsPassedByCourse(course);
+    }
+
+    @GetMapping("/course")
+    @ResponseBody
+    public Optional<Course> findCourseByDrivingLessonId(@RequestParam("id") Long id) {
+        return drivingLessonService.findCourseByDrivingLessonId(id);
+    }
+
+    @PostMapping("/add")
+    public ResponseEntity<DrivingLesson> addDrivingLesson(@RequestBody DrivingLesson lesson,
+                                                          @RequestParam("id") Long id) {
+        return drivingLessonService.addDrivingLesson(lesson, id);
+    }
+
+    @PutMapping("/changeStatus")
+    public ResponseEntity<DrivingLesson> changeLessonStatusByDrivingLessonId(@RequestParam("id") Long id,
+                                                                             @RequestBody LessonStatus status) {
+        return drivingLessonService.changeLessonStatusByDrivingLessonId(id, status);
     }
 
 }
