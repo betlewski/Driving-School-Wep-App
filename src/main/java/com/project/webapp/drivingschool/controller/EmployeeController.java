be17b3f184fa4b2c70bb643fbb1d3fa6c1078644
betlewski <szymonbetlewski@wp.pl>
2020-10-20 23:37:15 +0200

    Used service methods in employee controller

--- src/main/java/com/project/webapp/drivingschool/controller/EmployeeController.java
+++ src/main/java/com/project/webapp/drivingschool/controller/EmployeeController.java
@@ -1,10 +1,13 @@
 package com.project.webapp.drivingschool.controller;
 
+import com.project.webapp.drivingschool.model.Employee;
 import com.project.webapp.drivingschool.service.EmployeeService;
+import com.project.webapp.drivingschool.utils.EmployeeRole;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 /**
  * Kontroler dla pracowników szkoły nauki jazdy
@@ -19,6 +22,78 @@ public class EmployeeController {
     @Autowired
     public EmployeeController(EmployeeService employeeService) {
         this.employeeService = employeeService;
+    }
+
+    @GetMapping("/allActual")
+    @ResponseBody
+    public List<Employee> getAllActualEmployees() {
+        return employeeService.getAllActualEmployees();
+    }
+
+    @GetMapping("/all")
+    @ResponseBody
+    public List<Employee> getAllEmployees() {
+        return employeeService.getAllEmployees();
+    }
+
+    @GetMapping("/allNotAdmins")
+    @ResponseBody
+    public List<Employee> getAllEmployeesNotAdmins() {
+        return employeeService.getAllEmployeesNotAdmins();
+    }
+
+    @GetMapping("/allByRole")
+    @ResponseBody
+    public List<Employee> getAllEmployeesByEmployeeRole(@RequestParam("role") EmployeeRole role) {
+        return employeeService.getAllEmployeesByEmployeeRole(role);
+    }
+
+    @GetMapping("/byEmail")
+    @ResponseBody
+    public Employee getEmployeeByEmail(@RequestParam("email") String email) {
+        return employeeService.getEmployeeByEmail(email);
+    }
+
+    @GetMapping("/allRoles")
+    @ResponseBody
+    public List<EmployeeRole> getAllEmployeeRoles() {
+        return employeeService.getAllEmployeeRoles();
+    }
+
+    @GetMapping("/checkByEmployeeIdAndRole")
+    @ResponseBody
+    public Boolean checkExistingByEmployeeIdAndEmployeeRole(@RequestParam("id") Long id,
+                                                            @RequestParam("role") EmployeeRole role) {
+        return employeeService.checkExistingByEmployeeIdAndEmployeeRole(id, role);
+    }
+
+    @GetMapping("/checkEmail")
+    @ResponseBody
+    public Boolean emailExisting(@RequestParam("email") String email) {
+        return employeeService.emailExisting(email);
+    }
+
+    @PostMapping("/add")
+    public ResponseEntity<Employee> addEmployee(@RequestBody Employee employee) {
+        return employeeService.addEmployee(employee);
+    }
+
+    @PutMapping("/edit")
+    public ResponseEntity<Employee> editEmployee(@RequestParam("email") String email,
+                                                 @RequestBody Employee newEmployee) {
+        return employeeService.editEmployee(email, newEmployee);
+    }
+
+    @PutMapping("/changePassword")
+    public ResponseEntity<Employee> changePassword(@RequestParam("email") String email,
+                                                   @RequestBody String newPassword) {
+        return employeeService.changePassword(email, newPassword);
+    }
+
+    @PutMapping("/changeRole")
+    public ResponseEntity<Employee> changeEmployeeRole(@RequestParam("email") String email,
+                                                       @RequestBody EmployeeRole newRole) {
+        return employeeService.changeEmployeeRole(email, newRole);
     }
 
 }
