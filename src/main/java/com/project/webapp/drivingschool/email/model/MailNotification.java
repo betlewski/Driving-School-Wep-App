c78d7f1b81a03a2305415bb38ea5c419d8a3fda6
betlewski <szymonbetlewski@wp.pl>
2020-11-08 11:03:51 +0100

    Added mail notification model class

---
+++ src/main/java/com/project/webapp/drivingschool/email/model/MailNotification.java
@@ -0,0 +1,30 @@
+package com.project.webapp.drivingschool.email.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * Klasa reprezentująca powiadomienie mailowe
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class MailNotification {
+
+    /**
+     * Odbiorca powiadomienia
+     */
+    private String mailTo;
+
+    /**
+     * Temat powiadomienia
+     */
+    private String subject;
+
+    /**
+     * Tekst wiadomości
+     */
+    private String text;
+
+}
