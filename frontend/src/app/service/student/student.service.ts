7dda37e986fbbd1e691903da53b9f2c925881610
betlewski <szymonbetlewski@wp.pl>
2020-11-20 20:28:22 +0100

    Added service class for student data

---
+++ frontend/src/app/service/student/student.service.ts
@@ -0,0 +1,24 @@
+import {Injectable} from '@angular/core';
+import {environment} from "../../../environments/environment";
+import {Student} from "../../model/student.model";
+import {HttpClient} from "@angular/common/http";
+
+@Injectable({
+  providedIn: 'root'
+})
+/**
+ * Serwis do zarządzania kursantami
+ */
+export class StudentService {
+
+  private STUDENT_URL = environment.restUrl + '/student';
+  private REGISTER_URL = this.STUDENT_URL + '/add';
+
+  constructor(private http: HttpClient) {
+  }
+
+  public register(student: Student) {
+    return this.http.post<Student>(this.REGISTER_URL, student);
+  }
+
+}
