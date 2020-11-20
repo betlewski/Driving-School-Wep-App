944e92e6e6de1677518bb8c5f677cb9607df6d67
betlewski <szymonbetlewski@wp.pl>
2020-11-20 20:25:50 +0100

    Added utils of checking if user data are correct

--- frontend/src/app/utils/utils.ts
+++ frontend/src/app/utils/utils.ts
@@ -7,4 +7,14 @@ export class Utils {
     return data != null && data.trim() != "";
   }
 
+  public static checkIfEmailCorrect(email: string): boolean {
+    return email == null ? false :
+      email.match("^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,6}$") != null;
+  }
+
+  public static checkIfPasswordCorrect(password: string): boolean {
+    return password == null ? false :
+      password.match("^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=\\S+$).{8,}$") != null;
+  }
+
 }
