298b77f5f6e17d830b23f9030829f96015a01f09
betlewski <szymonbetlewski@wp.pl>
2020-11-27 20:48:04 +0100

    Added home panel for student and checking which tabs to open according to active course

---
+++ frontend/src/app/component/separate/student/home/home-student.component.ts
@@ -0,0 +1,67 @@
+import {Component, OnInit} from '@angular/core';
+import {AuthService} from "../../../../service/auth/auth.service";
+import {CourseService} from "../../../../service/rest/course/course.service";
+import {CourseStatus} from "../../../../utils/course-status";
+import {Router} from "@angular/router";
+
+@Component({
+  selector: 'app-home-student',
+  templateUrl: './home-student.component.html',
+  styleUrls: ['./home-student.component.css']
+})
+/**
+ * Panel główny kursanta
+ */
+export class HomeStudentComponent implements OnInit {
+
+  email = "";
+  isActiveCourse = false;
+  theoryEnable = false;
+  drivingEnable = false;
+  examEnable = false;
+
+  constructor(private router: Router,
+              private authService: AuthService,
+              private courseService: CourseService) {
+  }
+
+  ngOnInit(): void {
+    this.email = this.authService.getUserEmail();
+    this.initDashboardTabs();
+  }
+
+  private initDashboardTabs() {
+    this.courseService.findActiveCourseByEmail(this.email)
+      .subscribe(course => {
+        if (course != null && course.courseStatus != null) {
+          this.isActiveCourse = true;
+          switch (CourseStatus[course.courseStatus] as unknown) {
+            case CourseStatus.LECTURES:
+              this.setDashboardTabs(true, false, false);
+              break;
+            case CourseStatus.THEORY_INTERNAL_EXAM:
+              this.setDashboardTabs(true, false, true);
+              break;
+            case CourseStatus.DRIVING_LESSONS:
+            case CourseStatus.PRACTICAL_INTERNAL_EXAM:
+            case CourseStatus.STATE_EXAMS:
+            case CourseStatus.FINISHED:
+              this.setDashboardTabs(true, true, true);
+              break;
+          }
+        }
+      });
+  }
+
+  private setDashboardTabs(theoryEnable: boolean, drivingEnable: boolean, examEnable: boolean) {
+    this.theoryEnable = theoryEnable;
+    this.drivingEnable = drivingEnable;
+    this.examEnable = examEnable;
+  }
+
+  public logout() {
+    this.authService.clearItems();
+    this.router.navigate(["/login"]);
+  }
+
+}
