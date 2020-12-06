0d256bb233564d1c62b94e8cd084c6102d7cf9e7
betlewski <szymonbetlewski@wp.pl>
2020-12-06 19:41:36 +0100

    Added driving lesson service

---
+++ frontend/src/app/service/rest/driving-lesson/driving-lesson.service.ts
@@ -0,0 +1,30 @@
+import {Injectable} from '@angular/core';
+import {environment} from "../../../../environments/environment";
+import {HttpClient, HttpParams} from "@angular/common/http";
+import {AuthService} from "../../auth/auth.service";
+import {Observable} from "rxjs";
+import {DrivingLesson} from "../../../model/driving-lesson.model";
+
+@Injectable({
+  providedIn: 'root'
+})
+/**
+ * Serwis do zarządzania jazdami szkoleniowymi.
+ */
+export class DrivingLessonService {
+
+  private DRIVING_LESSON_URL = environment.restUrl + '/driving';
+  private FIND_ALL_BY_EMAIL_URL = this.DRIVING_LESSON_URL + '/all/byEmail';
+
+  constructor(private http: HttpClient,
+              private authService: AuthService) {
+  }
+
+  public findAllByEmail(email: string): Observable<DrivingLesson[]> {
+    const headers = this.authService.getAuthHeaders();
+    const params = new HttpParams().set("email", email);
+    return this.http.get<DrivingLesson[]>(this.FIND_ALL_BY_EMAIL_URL,
+      {headers: headers, params: params});
+  }
+
+}
