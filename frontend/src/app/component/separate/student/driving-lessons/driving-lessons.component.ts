2ebfc21edde6317ab5dc4810e06f169032d865c0
betlewski <szymonbetlewski@wp.pl>
2020-12-09 22:53:28 +0100

    Changed driving lessons component for student

--- frontend/src/app/component/separate/student/driving-lessons/driving-lessons.component.ts
+++ frontend/src/app/component/separate/student/driving-lessons/driving-lessons.component.ts
@@ -4,6 +4,9 @@ import {DrivingLessonService} from "../../../../service/rest/driving-lesson/driv
 import {DrivingLesson} from "../../../../model/driving-lesson.model";
 import {LessonStatus} from "../../../../utils/lesson-status";
 import {Employee} from "../../../../model/employee.model";
+import {EmployeeService} from "../../../../service/rest/employee/employee.service";
+import {CourseService} from "../../../../service/rest/course/course.service";
+import {EmployeeRole} from "../../../../utils/employee-role";
 
 @Component({
   selector: 'app-driving-lessons',
@@ -19,13 +22,33 @@ export class DrivingLessonsComponent implements OnInit {
   passedLessons: DrivingLesson[] = [];
   failedLessons: DrivingLesson[] = [];
 
+  instructors: Employee[] = [];
+  instructor: Employee | null = null;
+  startTime: Date | null = null;
+  endTime: Date | null = null;
+
+  feedback: string = "";
+
   constructor(private authService: AuthService,
-              private drivingLessonService: DrivingLessonService) {
+              private drivingLessonService: DrivingLessonService,
+              private employeeService: EmployeeService,
+              private courseService: CourseService) {
   }
 
   ngOnInit(): void {
     const email = this.authService.getUserEmail();
+    this.getAllInstructorsByCourseCategory(email);
     this.getAllDrivingLessons(email);
+  }
+
+  private getAllInstructorsByCourseCategory(email: string) {
+    this.courseService.findActiveCourseByEmail(email).subscribe(course => {
+      if (course != null && course.licenseCategory != null) {
+        const role = EmployeeRole.getInstructorRoleByCourseCategory(course.licenseCategory);
+        this.employeeService.findAllByRole(role).subscribe(
+          employees => this.instructors = employees)
+      }
+    });
   }
 
   private getAllDrivingLessons(email: string) {
@@ -35,20 +58,23 @@ export class DrivingLessonsComponent implements OnInit {
 
   private sortLessonsByStatus(lessons: DrivingLesson[]): void {
     lessons.forEach(lesson => {
-      switch (LessonStatus[lesson.lessonStatus] as unknown) {
-        case LessonStatus.REQUESTED:
-        case LessonStatus.ACCEPTED:
-          this.waitingLessons.push(lesson);
-          break;
-        case LessonStatus.PASSED:
-          this.passedLessons.push(lesson);
-          break;
-        case LessonStatus.REJECTED:
-        case LessonStatus.FAILED:
-          this.failedLessons.push(lesson);
-          break;
-        default:
-          break;
+      const lessonStatus = lesson.lessonStatus;
+      if (lessonStatus != null) {
+        switch (LessonStatus[lessonStatus] as unknown) {
+          case LessonStatus.REQUESTED:
+          case LessonStatus.ACCEPTED:
+            this.waitingLessons.push(lesson);
+            break;
+          case LessonStatus.PASSED:
+            this.passedLessons.push(lesson);
+            break;
+          case LessonStatus.REJECTED:
+          case LessonStatus.FAILED:
+            this.failedLessons.push(lesson);
+            break;
+          default:
+            break;
+        }
       }
     });
     this.sortLessonsByStartTime(this.waitingLessons);
@@ -58,12 +84,12 @@ export class DrivingLessonsComponent implements OnInit {
 
   private sortLessonsByStartTime(lessons: DrivingLesson[]): void {
     lessons.sort((a, b) =>
-      a.startTime.getTime() - b.startTime.getTime());
+      new Date(a.startTime).getTime() - new Date(b.startTime).getTime());
   }
 
   public convertEmployee(employee: Employee): string {
     return employee.fullName.concat(" (")
-      .concat(employee.phoneNumber).concat(")");
+      .concat(employee.email).concat(")");
   }
 
   public translateStatus(status: LessonStatus): string {
@@ -74,6 +100,43 @@ export class DrivingLessonsComponent implements OnInit {
     const dateToParse = new Date(date);
     return dateToParse.toLocaleDateString().concat("  ")
       .concat(dateToParse.toLocaleTimeString());
+  }
+
+  public request(): void {
+    // @ts-ignore
+    if (this.instructor != null && this.instructor != "" && this.startTime != null && this.endTime != null) {
+      const studentEmail = this.authService.getUserEmail();
+      const employeeEmail = this.instructor.email;
+      const lesson = new DrivingLesson(null, null, null, this.startTime, this.endTime);
+      this.drivingLessonService.addLesson(studentEmail, employeeEmail, lesson).subscribe(
+        () => {
+          this.refreshData();
+          this.feedback = "Nowe zgłoszenie znajdziesz w tabeli poniżej - zaczekaj, aż instruktor je zaakceptuje.";
+        },
+        error => {
+          if (error.status == 400) {
+            this.feedback = "Czas rozpoczęcia (najwcześniej 6:00) musi następować po czasie zakończenia (najpóźniej 20:00)";
+          }
+        });
+    } else {
+      this.feedback = "Nie wybrano wszystkich danych.";
+    }
+  }
+
+  private refreshData(): void {
+    this.cleanData();
+    this.ngOnInit();
+  }
+
+  private cleanData(): void {
+    this.waitingLessons = [];
+    this.passedLessons = [];
+    this.failedLessons = [];
+    this.instructors = [];
+    this.instructor = null;
+    this.startTime = null;
+    this.endTime = null;
+    this.feedback = "";
   }
 
 }
