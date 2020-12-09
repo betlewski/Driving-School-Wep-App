e1e82105c3da848e526607b1590a253e1ab667b1
betlewski <szymonbetlewski@wp.pl>
2020-12-09 22:51:37 +0100

    Added method adding driving lesson in service

--- frontend/src/app/service/rest/driving-lesson/driving-lesson.service.ts
+++ frontend/src/app/service/rest/driving-lesson/driving-lesson.service.ts
@@ -15,6 +15,7 @@ export class DrivingLessonService {
 
   private DRIVING_LESSON_URL = environment.restUrl + '/driving';
   private FIND_ALL_BY_EMAIL_URL = this.DRIVING_LESSON_URL + '/all/byEmail';
+  private ADD_LESSON_URL = this.DRIVING_LESSON_URL + '/add';
 
   constructor(private http: HttpClient,
               private authService: AuthService) {
@@ -24,6 +25,15 @@ export class DrivingLessonService {
     const headers = this.authService.getAuthHeaders();
     const params = new HttpParams().set("email", email);
     return this.http.get<DrivingLesson[]>(this.FIND_ALL_BY_EMAIL_URL,
+      {headers: headers, params: params});
+  }
+
+  public addLesson(studentEmail: string, employeeEmail: string, lesson: DrivingLesson): Observable<DrivingLesson> {
+    const headers = this.authService.getAuthHeaders();
+    const params = new HttpParams()
+      .set("student", studentEmail)
+      .set("employee", employeeEmail);
+    return this.http.post<DrivingLesson>(this.ADD_LESSON_URL, lesson,
       {headers: headers, params: params});
   }
 
