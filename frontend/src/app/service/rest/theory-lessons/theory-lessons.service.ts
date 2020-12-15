354e43ae47add67df10bf2667ea344069773b409
betlewski <szymonbetlewski@wp.pl>
2020-12-16 00:04:23 +0100

    Added method of finding active theory lessons in rest service

--- frontend/src/app/service/rest/theory-lessons/theory-lessons.service.ts
+++ frontend/src/app/service/rest/theory-lessons/theory-lessons.service.ts
@@ -5,6 +5,7 @@ import {AuthService} from "../../auth/auth.service";
 import {Observable} from "rxjs";
 import {Lecture} from "../../../model/lecture.model";
 import {TheoryLessons} from "../../../model/theory-lessons.model";
+import {LessonStatus} from "../../../utils/lesson-status";
 
 @Injectable({
   providedIn: 'root'
@@ -18,6 +19,8 @@ export class TheoryLessonsService {
   private FIND_ALL_LECTURES_BY_EMAIL_URL = this.THEORY_LESSONS_URL + '/lectures/all/byEmail';
   private IS_ACTIVE_BY_EMAIL_URL = this.THEORY_LESSONS_URL + '/isActive/byEmail';
   private ADD_URL = this.THEORY_LESSONS_URL + '/add';
+  private FIND_ALL_BY_EMAIL_AND_STATUS_URL = this.THEORY_LESSONS_URL + '/all/byEmail/byStatus';
+  private FIND_ACTIVE_BY_EMAIL_URL = this.THEORY_LESSONS_URL + '/active/byEmail';
 
   constructor(private http: HttpClient,
               private authService: AuthService) {
@@ -41,6 +44,22 @@ export class TheoryLessonsService {
     const headers = this.authService.getAuthHeaders();
     const params = new HttpParams().set("email", email);
     return this.http.post<TheoryLessons>(this.ADD_URL, seriesId,
+      {headers: headers, params: params});
+  }
+
+  public findAllByEmailAndStatus(email: string, status: LessonStatus): Observable<TheoryLessons[]> {
+    const headers = this.authService.getAuthHeaders();
+    const params = new HttpParams()
+      .set("email", email)
+      .set("status", LessonStatus[status]);
+    return this.http.get<TheoryLessons[]>(this.FIND_ALL_BY_EMAIL_AND_STATUS_URL,
+      {headers: headers, params: params});
+  }
+
+  public findActiveByEmail(email: string): Observable<TheoryLessons> {
+    const headers = this.authService.getAuthHeaders();
+    const params = new HttpParams().set("email", email);
+    return this.http.get<TheoryLessons>(this.FIND_ACTIVE_BY_EMAIL_URL,
       {headers: headers, params: params});
   }
 
