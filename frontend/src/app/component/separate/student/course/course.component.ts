e2abbd635e432fea0d5c0138fd85827b7b5fd302
betlewski <szymonbetlewski@wp.pl>
2020-12-18 11:14:45 +0100

    Added charts and table data in course component

--- frontend/src/app/component/separate/student/course/course.component.ts
+++ frontend/src/app/component/separate/student/course/course.component.ts
@@ -1,8 +1,12 @@
 import {Component, OnInit} from '@angular/core';
 import {CourseService} from "../../../../service/rest/course/course.service";
 import {AuthService} from "../../../../service/auth/auth.service";
-import {CourseReport} from "../../../../model/course-report.model";
 import {CourseStatus} from "../../../../utils/course-status";
+// @ts-ignore
+import * as CanvasJS from "../../../../../assets/canvas-js/canvasjs.min";
+import {CourseReport} from "../../../../model/course-report.model";
+
+CanvasJS.addColorSet("colorSet", ["#5b616f", "#f1f2f3"]);
 
 @Component({
   selector: 'app-course',
@@ -14,11 +18,15 @@ import {CourseStatus} from "../../../../utils/course-status";
  */
 export class CourseComponent implements OnInit {
 
-  courseReport: CourseReport | null = null;
+  comment: string = "";
   passedCoursePercent: number = 0;
+  extraDrivingLessonsHours: number = 0;
+  startTime: Date | null = null;
+  endTime: Date | null = null;
 
-  imagePath = "../../../../../assets/course-status/";
+  private IMAGE_PATH = "../../../../../assets/course-status/";
   imageSrc: string = "";
+  chart: CanvasJS;
 
   constructor(private authService: AuthService,
               private courseService: CourseService) {
@@ -29,16 +37,51 @@ export class CourseComponent implements OnInit {
     this.courseService.getReportByEmail(email)
       .subscribe(course => {
         if (course != null) {
-          this.courseReport = course;
-          this.passedCoursePercent = course.passedCoursePercent;
+          this.getDisplayedData(course);
           this.showStatusImage(course.courseStatus);
+          this.drawTheoryChart(course.passedTheoryHours, course.theoryHours);
+          this.drawPracticeChart(course.passedPracticeHours, course.practiceHours);
+        } else {
+          this.comment = "Nie znaleziono aktywnego kursu. Rozpocznij nowy...";
         }
       });
   }
 
+  private getDisplayedData(course: CourseReport): void {
+    this.comment = course.comment;
+    this.passedCoursePercent = course.passedCoursePercent;
+    this.extraDrivingLessonsHours = course.extraDrivingLessonsHours;
+    this.startTime = course.startDate;
+    this.endTime = course.endTime;
+  }
+
   private showStatusImage(status: CourseStatus): void {
     const imageNumber = CourseStatus.getImageNumber(status);
-    this.imageSrc = this.imagePath + imageNumber + ".jpg";
+    this.imageSrc = this.IMAGE_PATH + imageNumber + ".jpg";
+  }
+
+  private drawTheoryChart(passedTheoryHours: number, theoryHours: number): void {
+    this.drawChart(passedTheoryHours, theoryHours, "theoryChart");
+  }
+
+  private drawPracticeChart(passedPracticeHours: number, practiceHours: number): void {
+    this.drawChart(passedPracticeHours, practiceHours, "practiceChart");
+  }
+
+  private drawChart(passedHours: number, allHours: number, chartId: string) {
+    this.chart = new CanvasJS.Chart(chartId, {
+      animationEnabled: true,
+      colorSet: "colorSet",
+      data: [{
+        type: "pie",
+        toolTipContent: "<b>{name}</b>: {y}h",
+        dataPoints: [
+          {y: passedHours, name: "Ukończono"},
+          {y: allHours - passedHours, name: "Pozostało"},
+        ]
+      }]
+    });
+    this.chart.render();
   }
 
 }
