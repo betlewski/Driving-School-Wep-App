f20a01290abbbc0922a6836fca0de4fc727c28ae
betlewski <szymonbetlewski@wp.pl>
2020-12-18 19:39:51 +0100

    Added findByEmail and edit method in employee service

--- frontend/src/app/service/rest/employee/employee.service.ts
+++ frontend/src/app/service/rest/employee/employee.service.ts
@@ -16,6 +16,8 @@ export class EmployeeService {
 
   private EMPLOYEE_URL = environment.restUrl + '/employee';
   private FIND_ALL_BY_ROLE = this.EMPLOYEE_URL + '/all/byRole';
+  private FIND_BY_EMAIL_URL = this.EMPLOYEE_URL + '/byEmail';
+  private EDIT_URL = this.EMPLOYEE_URL + '/edit';
 
   constructor(private http: HttpClient,
               private authService: AuthService) {
@@ -25,6 +27,20 @@ export class EmployeeService {
     const headers = this.authService.getAuthHeaders();
     const params = new HttpParams().set("role", EmployeeRole[role]);
     return this.http.get<Employee[]>(this.FIND_ALL_BY_ROLE,
+      {headers: headers, params: params});
+  }
+
+  public findByEmail(email: string): Observable<Employee> {
+    const headers = this.authService.getAuthHeaders();
+    const params = new HttpParams().set("email", email);
+    return this.http.get<Employee>(this.FIND_BY_EMAIL_URL,
+      {headers: headers, params: params});
+  }
+
+  public edit(email: string, newEmployee: Employee): Observable<Employee> {
+    const headers = this.authService.getAuthHeaders();
+    const params = new HttpParams().set("email", email);
+    return this.http.put<Employee>(this.EDIT_URL, newEmployee,
       {headers: headers, params: params});
   }
 
