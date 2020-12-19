6566d96fb9d92776f6a832b304a1337ac263aabd
betlewski <szymonbetlewski@wp.pl>
2020-12-19 21:40:23 +0100

    Added translating method in employee roles enum

--- frontend/src/app/utils/employee-role.ts
+++ frontend/src/app/utils/employee-role.ts
@@ -16,6 +16,42 @@ export enum EmployeeRole {
 
 export namespace EmployeeRole {
 
+  const ADMINISTRATOR_TRANSLATION = "Administrator";
+  const DRIVING_INSTRUCTOR_A_TRANSLATION = "Instruktor A";
+  const DRIVING_INSTRUCTOR_B_TRANSLATION = "Instruktor B";
+  const LECTURER_TRANSLATION = "Wykładowca";
+  const DELETED_TRANSLATION = "Usunięty";
+
+  export function translate(role: EmployeeRole): string {
+    let translator: string;
+    switch (role) {
+      case EmployeeRole.ADMINISTRATOR:
+        translator = ADMINISTRATOR_TRANSLATION;
+        break;
+      case EmployeeRole.DRIVING_INSTRUCTOR_A:
+        translator = DRIVING_INSTRUCTOR_A_TRANSLATION;
+        break;
+      case EmployeeRole.DRIVING_INSTRUCTOR_B:
+        translator = DRIVING_INSTRUCTOR_B_TRANSLATION;
+        break;
+      case EmployeeRole.LECTURER:
+        translator = LECTURER_TRANSLATION;
+        break;
+      case EmployeeRole.DELETED:
+        translator = DELETED_TRANSLATION;
+        break;
+      default:
+        translator = "";
+        break;
+    }
+    return translator;
+  }
+
+  export function values(): EmployeeRole[] {
+    return [EmployeeRole.ADMINISTRATOR, EmployeeRole.DRIVING_INSTRUCTOR_A,
+      EmployeeRole.DRIVING_INSTRUCTOR_B, EmployeeRole.LECTURER, EmployeeRole.DELETED];
+  }
+
   export function getInstructorRoleByCourseCategory(category: LicenseCategory): EmployeeRole {
     switch (category.toString()) {
       case "AM":
