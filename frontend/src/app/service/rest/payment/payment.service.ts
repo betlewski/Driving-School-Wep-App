3c71381cffd0fdce18f7ebd713ed38f0acc81596
betlewski <szymonbetlewski@wp.pl>
2020-12-19 13:41:07 +0100

    Added accepting and rejecting methods in document and payment services

--- frontend/src/app/service/rest/payment/payment.service.ts
+++ frontend/src/app/service/rest/payment/payment.service.ts
@@ -4,6 +4,7 @@ import {HttpClient, HttpParams} from "@angular/common/http";
 import {AuthService} from "../../auth/auth.service";
 import {Observable} from "rxjs";
 import {Payment} from "../../../model/payment.model";
+import {ProcessingStatus} from "../../../utils/processing-status";
 
 @Injectable({
   providedIn: 'root'
@@ -16,6 +17,7 @@ export class PaymentService {
   private PAYMENT_URL = environment.restUrl + '/payment';
   private FIND_ALL_BY_EMAIL_URL = this.PAYMENT_URL + '/all/byEmail';
   private REQUEST_BY_ID_URL = this.PAYMENT_URL + '/edit/request';
+  private EDIT_STATUS_BY_ID_URL = this.PAYMENT_URL + '/edit/status';
 
   constructor(private http: HttpClient,
               private authService: AuthService) {
@@ -32,6 +34,21 @@ export class PaymentService {
     const headers = this.authService.getAuthHeaders();
     return this.http.put<Payment>(this.REQUEST_BY_ID_URL, paymentId,
       {headers: headers});
+  }
+
+  public editStatus(paymentId: number, status: ProcessingStatus): Observable<Payment> {
+    const headers = this.authService.getAuthHeaders();
+    const params = new HttpParams().set("id", paymentId.toString());
+    return this.http.put<Payment>(this.EDIT_STATUS_BY_ID_URL, status,
+      {headers: headers, params: params});
+  }
+
+  public accept(paymentId: number): Observable<Payment> {
+    return this.editStatus(paymentId, ProcessingStatus.COMPLETED);
+  }
+
+  public reject(paymentId: number): Observable<Payment> {
+    return this.editStatus(paymentId, ProcessingStatus.TO_COMPLETE);
   }
 
 }
