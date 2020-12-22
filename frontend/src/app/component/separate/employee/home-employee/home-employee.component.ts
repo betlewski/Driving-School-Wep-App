b6494fcb560af59edcf6f38cff865f8f0bb450c8
betlewski <szymonbetlewski@wp.pl>
2020-12-22 18:53:34 +0100

    Changed home component in employee panel

--- frontend/src/app/component/separate/employee/home-employee/home-employee.component.ts
+++ frontend/src/app/component/separate/employee/home-employee/home-employee.component.ts
@@ -1,5 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {AuthService} from "../../../../service/auth/auth.service";
+import {Router} from "@angular/router";
+import {UserRole} from "../../../../utils/user-role";
 
 @Component({
   selector: 'app-home-employee',
@@ -7,17 +9,35 @@ import {AuthService} from "../../../../service/auth/auth.service";
   styleUrls: ['./home-employee.component.css']
 })
 /**
- * Panel główny pracownika
+ * Panel główny pracownika.
  */
 export class HomeEmployeeComponent implements OnInit {
 
   email = "";
+  lecturerLogged: boolean = false;
+  instructorLogged: boolean = false;
 
-  constructor(private authService: AuthService) {
+  constructor(private router: Router,
+              private authService: AuthService) {
   }
 
   ngOnInit(): void {
     this.email = this.authService.getUserEmail();
+    this.setUserLogged();
+  }
+
+  private setUserLogged(): void {
+    const userRole = this.authService.getUserRole();
+    if (userRole == UserRole.LECTURER) {
+      this.lecturerLogged = true;
+    } else if (userRole == UserRole.INSTRUCTOR) {
+      this.instructorLogged = true;
+    }
+  }
+
+  public logout() {
+    this.authService.clearItems();
+    this.router.navigate(["/login"]);
   }
 
 }
