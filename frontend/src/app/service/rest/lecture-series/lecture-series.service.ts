93b2a785a4e836d2878464e65109e783ecc333e6
betlewski <szymonbetlewski@wp.pl>
2020-12-27 17:32:55 +0100

    Added new methods in lecture series service

--- frontend/src/app/service/rest/lecture-series/lecture-series.service.ts
+++ frontend/src/app/service/rest/lecture-series/lecture-series.service.ts
@@ -5,6 +5,7 @@ import {environment} from "../../../../environments/environment";
 import {Observable} from "rxjs";
 import {LectureSeries} from "../../../model/lecture-series.model";
 import {Lecture} from "../../../model/lecture.model";
+import {LectureSeriesStatus} from "../../../utils/lecture-series-status";
 
 @Injectable({
   providedIn: 'root'
@@ -16,7 +17,9 @@ export class LectureSeriesService {
 
   private LECTURE_SERIES_URL = environment.restUrl + '/series';
   private FIND_ALL_FREE_SERIES_URL = this.LECTURE_SERIES_URL + '/all/free';
+  private FIND_ALL_SERIES_BY_EMPLOYEE_URL = this.LECTURE_SERIES_URL + '/all/byEmployee';
   private FIND_ALL_LECTURES_BY_EMPLOYEE_URL = this.LECTURE_SERIES_URL + '/lectures/all/byEmployee';
+  private EDIT_SERIES_STATUS_URL = this.LECTURE_SERIES_URL + '/edit/status';
 
   constructor(private http: HttpClient,
               private authService: AuthService) {
@@ -28,10 +31,24 @@ export class LectureSeriesService {
       {headers: headers});
   }
 
+  public findAllSeriesByEmployee(email: string): Observable<LectureSeries[]> {
+    const headers = this.authService.getAuthHeaders();
+    const params = new HttpParams().set("email", email);
+    return this.http.get<LectureSeries[]>(this.FIND_ALL_SERIES_BY_EMPLOYEE_URL,
+      {headers: headers, params: params});
+  }
+
   public findAllLecturesByEmployee(email: string): Observable<Lecture[]> {
     const headers = this.authService.getAuthHeaders();
     const params = new HttpParams().set("email", email);
     return this.http.get<Lecture[]>(this.FIND_ALL_LECTURES_BY_EMPLOYEE_URL,
+      {headers: headers, params: params});
+  }
+
+  public editSeriesStatus(id: number, status: LectureSeriesStatus): Observable<LectureSeries> {
+    const headers = this.authService.getAuthHeaders();
+    const params = new HttpParams().set("id", id.toString());
+    return this.http.put<LectureSeries>(this.EDIT_SERIES_STATUS_URL, JSON.stringify(status),
       {headers: headers, params: params});
   }
 
